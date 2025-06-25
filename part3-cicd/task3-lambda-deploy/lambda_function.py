d3f0a3d6bb0356ce42645f20cb8dfe686ecc4eab
afamokd989 <afamokd@yahoo.com>
2025-06-25 19:29:43 -0400

    fixed linting issues

--- part3-cicd/task3-lambda-deploy/lambda_function.py
+++ part3-cicd/task3-lambda-deploy/lambda_function.py
@@ -1,8 +1,21 @@
+"""
+Lambda function module for Afam Financial.
+"""
+
 import json
 
 
-# This is the Lambda function code for the Afam Financial project.
 def lambda_handler(event, context):
+    """
+    AWS Lambda entry point.
+
+    Parameters:
+    - event: dict, AWS Lambda uses this parameter to pass in event data.
+    - context: object, AWS Lambda uses this parameter to provide runtime information.
+
+    Returns:
+    - dict: response with HTTP status and body
+    """
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Afam Financial Lambda!')
